25b807aa9eeb3f95f2ed2e97a5e9cd85965ea3bc
dunzane <dengzhaowork@gmail.com>
2025-05-09 21:55:31 +0800

    fix:qps.sh

--- scripts/Autoformer/KQPS.sh
+++ scripts/Autoformer/KQPS.sh
@@ -2,16 +2,17 @@ export CUDA_VISIBLE_DEVICES=0
 
 model_name=Autoformer
 
+
 python -u run.py \
   --task_name long_term_forecast \
   --is_training 1 \
   --root_path ./dataset/Kuai/ \
   --data_path easy_train_data.csv \
-  --model_id Easy_QPS_1440_60 \
+  --model_id Easy_QPS_96_60 \
   --model $model_name \
   --data KuaiEasyQPS \
   --features S \
-  --seq_len 1440 \
+  --seq_len 96 \
   --label_len 48 \
   --pred_len 60 \
   --e_layers 2 \
@@ -20,6 +21,29 @@ python -u run.py \
   --enc_in 1 \
   --dec_in 1 \
   --c_out 1 \
-  --freq 'b' \
+  --freq 'd' \
   --des 'Exp' \
   --itr 1
+
+
+#python -u run.py \
+#  --task_name long_term_forecast \
+#  --is_training 1 \
+#  --root_path ./dataset/Kuai/ \
+#  --data_path easy_train_data.csv \
+#  --model_id Easy_QPS_1440_60 \
+#  --model $model_name \
+#  --data KuaiEasyQPS \
+#  --features S \
+#  --seq_len 1440 \
+#  --label_len 48 \
+#  --pred_len 60 \
+#  --e_layers 2 \
+#  --d_layers 1 \
+#  --factor 3 \
+#  --enc_in 1 \
+#  --dec_in 1 \
+#  --c_out 1 \
+#  --freq 'b' \
+#  --des 'Exp' \
+#  --itr 1
